997acd632a36b85ae9bc90a952cbc21b98825261
tomasote321 <118144731+tomasote321@users.noreply.github.com>
2025-05-23 19:04:04 -0500

    clase Excepciones

---
+++ POOBkemon/src/domain/POOBkemonException.java
@@ -0,0 +1,10 @@
+package domain;
+
+/**
+ * Clase base para todas las excepciones personalizadas del sistema POOBkemon.
+ */
+public class POOBkemonException extends Exception {
+    public POOBkemonException(String message) {
+        super(message);
+    }
+}
